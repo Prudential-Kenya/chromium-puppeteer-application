62d431ba00b0bfb9c963da44ef4398062c426fa4
Anthony Salemo <ajssalemo@gmail.com>
2023-05-05 13:43:38 -0400

    initial commit

---
+++ controllers/scrapeController.js
@@ -0,0 +1,34 @@
+import * as express from "express";
+import puppeteer from 'puppeteer';
+
+const router = express.Router()
+
+export const scrapeController = router.get("/", async (_req, res) => {
+    const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
+    const page = await browser.newPage();
+    
+    await page.goto('https://developer.chrome.com/');
+    
+    // Set screen size
+    await page.setViewport({width: 1080, height: 1024});
+    
+    // Type into search box
+    await page.type('.search-box__input', 'automate beyond recorder');
+    
+    // Wait and click on first result
+    const searchResultSelector = '.search-box__link';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+    
+    // Locate the full title with a unique string
+    const textSelector = await page.waitForSelector(
+    'text/Customize and automate'
+    );
+    const fullTitle = await textSelector.evaluate(el => el.textContent);
+    
+    // Print the full title
+    console.log('The title of this blog post is "%s".', fullTitle);
+    await browser.close();
+    res.json({ msg: fullTitle })
+})
+
